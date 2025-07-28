49d304425a8d65ae84f8197263ed2824f20791f8
fasilahammed <ahamedahamed1883@gmail.com>
2025-07-28 14:26:12 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,9 @@ function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path='/' element={< Home />} />
+      <Route path='/home' element={< Home />} />
       <Route path='/login' element={< Login />} />
-      <Route path='/register' element={< Register />} />
+      <Route path='/' element={< Register />} />
 
 
 
